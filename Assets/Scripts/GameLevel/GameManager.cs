7e2070b78d91a42aa8b3a7f630c97ee65c560ac4
ismailonur <ismailonur15@gmail.com>
2020-09-24 17:51:34 +0300

    gameLevel sahnesinde kareler ve soru oluşturuldu

---
+++ Assets/Scripts/GameLevel/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject karePrefab;
+    public GameObject soruPaneli;
+    public Transform karelerPaneli;
+
+    private GameObject[] karelerDizisi = new GameObject[25];
+
+    void Start()
+    {
+        soruPaneli.GetComponent<RectTransform>().localScale = Vector3.zero;
+
+        kareleriOlustur();
+    }
+
+    public void kareleriOlustur()
+    {
+        for(int i = 0; i < 25; i++)
+        {
+            GameObject kare = Instantiate(karePrefab, karelerPaneli);
+            karelerDizisi[i] = kare;
+        }
+        bolumDegerleriniYaz();
+
+        StartCoroutine(DoFadeRoutine());
+
+        Invoke("SoruPaneliniAc", 2f);
+    }
+
+    IEnumerator DoFadeRoutine()
+    {
+        foreach(var kare in karelerDizisi)
+        {
+            kare.GetComponent<CanvasGroup>().DOFade(1, .2f);
+
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    void bolumDegerleriniYaz()
+    {
+        foreach(var kare in karelerDizisi)
+        {
+            int rastgeleDeger = Random.Range(1, 13);
+
+            kare.transform.GetChild(0).GetComponent<Text>().text = rastgeleDeger.ToString(); // karenin içinde texte ulaşıp değiştirmemizi sağlar.
+        }
+    }
+
+    void SoruPaneliniAc()
+    {
+        soruPaneli.GetComponent<RectTransform>().DOScale(1, .5f).SetEase(Ease.OutBack);
+    }
+}
--- Assets/Scripts/MenuLevel/MenuManager.cs
+++ Assets/Scripts/MenuLevel/MenuManager.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject startButton, exitButton;
+
     void Start()
+    {
+        FadeOut();
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FadeOut()
     {
-        
+        startButton.GetComponent<CanvasGroup>().DOFade(1, 0.8f);
+        exitButton.GetComponent<CanvasGroup>().DOFade(1, 0.8f).SetDelay(.5f); // SetDelay komutu gecikme yapmasını sağlar kod 0.5 saniye bekledikten sonra çalışır.
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGameLevel()
+    {
+        SceneManager.LoadScene("gameLevel");
     }
 }
