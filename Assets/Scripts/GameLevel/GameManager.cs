dc3de7bc2ad7e9a55d48a1f3f17cbf257024d907
ismailonur <ismailonur15@gmail.com>
2020-09-25 13:06:17 +0300

    GameLevel sahnesi yapılıyor. Soruların gelmesi ve doğru yanlış olayı yapıldı

--- Assets/Scripts/GameLevel/GameManager.cs
+++ Assets/Scripts/GameLevel/GameManager.cs
@@ -10,10 +10,40 @@ public class GameManager : MonoBehaviour
     public GameObject soruPaneli;
     public Transform karelerPaneli;
 
+    public Text soruText;
+
     private GameObject[] karelerDizisi = new GameObject[25];
+
+    List<int> bolumDegerleriListesi = new List<int>();
+
+    int bolenSayi, bolunenSayi;
+    int kacinciSoru;
+    int butonDegeri;
+    int dogruSonuc;
+
+    int kalanHak;
+
+    bool butonaBasilsinmi;
+
+    string sorununZorlukDerecesi;
+
+    KalanHaklarManager kalanHaklarManager;
+
+    PuanManager puanManager;
+
+    private void Awake()
+    {
+        kalanHak = 3;
+
+        kalanHaklarManager = Object.FindObjectOfType<KalanHaklarManager>();
+        puanManager = Object.FindObjectOfType<PuanManager>();
+
+        kalanHaklarManager.KalanHaklariKontrolEt(kalanHak);
+    }
 
     void Start()
     {
+        butonaBasilsinmi = false;
         soruPaneli.GetComponent<RectTransform>().localScale = Vector3.zero;
 
         kareleriOlustur();
@@ -24,6 +54,7 @@ public class GameManager : MonoBehaviour
         for(int i = 0; i < 25; i++)
         {
             GameObject kare = Instantiate(karePrefab, karelerPaneli);
+            kare.transform.GetComponent<Button>().onClick.AddListener(() => ButonaBasildi());
             karelerDizisi[i] = kare;
         }
         bolumDegerleriniYaz();
@@ -31,6 +62,33 @@ public class GameManager : MonoBehaviour
         StartCoroutine(DoFadeRoutine());
 
         Invoke("SoruPaneliniAc", 2f);
+    }
+
+    void ButonaBasildi()
+    {
+        if (butonaBasilsinmi)
+        {
+            butonDegeri = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.transform.GetChild(0).GetComponent<Text>().text);
+
+            SonucuKontrolEt();
+        }
+    }
+
+    void SonucuKontrolEt()
+    {
+        if(butonDegeri == dogruSonuc)
+        {
+            puanManager.PuaniArttir(sorununZorlukDerecesi);
+
+            bolumDegerleriListesi.RemoveAt(kacinciSoru);
+
+            SoruPaneliniAc();
+        }
+        else
+        {
+            kalanHak--;
+            kalanHaklarManager.KalanHaklariKontrolEt(kalanHak);
+        }
     }
 
     IEnumerator DoFadeRoutine()
@@ -48,6 +106,7 @@ public class GameManager : MonoBehaviour
         foreach(var kare in karelerDizisi)
         {
             int rastgeleDeger = Random.Range(1, 13);
+            bolumDegerleriListesi.Add(rastgeleDeger);
 
             kare.transform.GetChild(0).GetComponent<Text>().text = rastgeleDeger.ToString(); // karenin içinde texte ulaşıp değiştirmemizi sağlar.
         }
@@ -55,6 +114,34 @@ public class GameManager : MonoBehaviour
 
     void SoruPaneliniAc()
     {
+        SoruyuSor();
+        butonaBasilsinmi = true;
         soruPaneli.GetComponent<RectTransform>().DOScale(1, .5f).SetEase(Ease.OutBack);
+    }
+    
+    void SoruyuSor()
+    {
+        bolenSayi = Random.Range(2, 11);
+
+        kacinciSoru = Random.Range(0, bolumDegerleriListesi.Count);
+
+        dogruSonuc = bolumDegerleriListesi[kacinciSoru];
+
+        bolunenSayi = bolenSayi * dogruSonuc;
+
+        if(bolunenSayi <= 40)
+        {
+            sorununZorlukDerecesi = "kolay";
+        }
+        else if(bolunenSayi > 40 && bolunenSayi <= 80)
+        {
+            sorununZorlukDerecesi = "orta";
+        }
+        else
+        {
+            sorununZorlukDerecesi = "zor";
+        }
+
+        soruText.text = bolunenSayi.ToString() + " / " + bolenSayi.ToString();
     }
 }
